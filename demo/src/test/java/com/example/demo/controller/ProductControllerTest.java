dbc4793b6f963ea4e2b27326ee89a7b7a1116d1e
Kristijan Jakimov <kiko.jakimov@gmail.com>
2025-03-26 18:51:34 +0100

    making commandLine Runner not run in test

--- demo/src/test/java/com/example/demo/controller/ProductControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/ProductControllerTest.java
@@ -6,17 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class ProductControllerTest {
-//    @Autowired
-//    MockMvc mockMvc;
-//    @Autowired
-//    ProductRepo productRepo;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ProductRepo productRepo;
 
     @Test
     @DirtiesContext
