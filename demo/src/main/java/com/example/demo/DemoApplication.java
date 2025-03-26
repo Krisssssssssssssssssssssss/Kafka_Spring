c6503f053d931f8776ed58e3df4428c10b8bb2aa
Kristijan Jakimov <kiko.jakimov@gmail.com>
2025-03-26 16:33:25 +0100

    Kafka producer added

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner (KafkaTemplate<String,String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("testTopic", "Hello Kafka");
+		};
+	}
 }
