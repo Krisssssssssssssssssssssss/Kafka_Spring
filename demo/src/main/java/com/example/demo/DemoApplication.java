89b43faff773be9418f7ae009b7be7302a9e8466
Kristijan Jakimov <kiko.jakimov@gmail.com>
2025-03-26 18:45:29 +0100

    making commandLine Runner not run in test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Profile("!test")
 	@Bean
 	CommandLineRunner commandLineRunner (KafkaTemplate<String,String> kafkaTemplate){
 		return args -> {
